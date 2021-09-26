734aa87f5d9b9d3b2bd7b4f0c789da53a547028f
lucasvieira@protonmail.com <lucasvieira@protonmail.com>
2021-09-25 23:49:36 -0300

    Adiciona cadastro de endereço

--- View/uFrmCadCliente.pas
+++ View/uFrmCadCliente.pas
@@ -5,7 +5,7 @@ interface
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants, 
   FMX.Types, FMX.Graphics, FMX.Controls, FMX.Forms, FMX.Dialogs, FMX.StdCtrls,
-  FMX.Controls.Presentation, FMX.Edit;
+  FMX.Controls.Presentation, FMX.Edit, FMX.ListBox;
 
 type
   TfrmCadCliente = class(TFrame)
@@ -19,6 +19,13 @@ type
     pnlTipoPessoa: TPanel;
     btnVoltar: TButton;
     btnCadastrar: TButton;
+    grpEndereco: TGroupBox;
+    edtLogradouro: TEdit;
+    edtComplemento: TEdit;
+    edtNumero: TEdit;
+    edtBairro: TEdit;
+    edtCidade: TEdit;
+    cmbUF: TComboBox;
     procedure btnVoltarClick(Sender: TObject);
     procedure btnCadastrarClick(Sender: TObject);
   private
@@ -59,15 +66,28 @@ var
    xBody: TJSONObject;
    xValores: TJSONValue;
    xMensagem: String;
+   xArray: TJSONArray;
+   xEndereco: TJSONObject;
 begin
    xMensagem := EmptyStr;
    xBody := nil;
    try
       xBody := TJSONObject.Create;
+      xEndereco := TJSONObject.Create;
+      xArray := TJSONArray.Create;
+      xArray.Add(xEndereco);
+
       xBody.AddPair('nome', edtNome.Text);
       xBody.AddPair('pj', rdPessoaJuridica.IsChecked);
       xBody.AddPair('docto', edtCpfCnpj.Text);
-      xBody.AddPair('enderecos', TJSONArray.Create);
+      xBody.AddPair('enderecos', xArray);
+
+      xEndereco.AddPair('logradouro', edtLogradouro.Text);
+      xEndereco.AddPair('numero', edtNumero.Text);
+      xEndereco.AddPair('complemento', edtComplemento.Text);
+      xEndereco.AddPair('bairro', edtBairro.Text);
+      xEndereco.AddPair('cidade', edtCidade.Text);
+      xEndereco.AddPair('uf', cmbUF.Items[cmbUF.ItemIndex]);
 
       xResposta := TMinervaRequest.get.SyncRequest(rmPOST, '/clientes', xBody);
 
